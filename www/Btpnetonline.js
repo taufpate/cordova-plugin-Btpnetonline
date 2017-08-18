89575dcd93e587c22c8ec9eb7523650b62a83ccf
gunes erdemi <guneserdemi@hotmail.com>
2017-08-18 13:44:36 +0300

    Update Btpnetonline.js

--- www/Btpnetonline.js
+++ www/Btpnetonline.js
@@ -1,5 +1,27 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "Btpnetonline", "coolMethod", [arg0]);
+var BTPrinter = {
+   list: function(fnSuccess, fnError){
+      exec(fnSuccess, fnError, "Btpnetonline", "list", []);
+   },
+   connect: function(fnSuccess, fnError, name){
+      exec(fnSuccess, fnError, "Btpnetonline", "connect", [name]);
+   },
+   disconnect: function(fnSuccess, fnError){
+      exec(fnSuccess, fnError, "Btpnetonline", "disconnect", []);
+   },
+   print: function(fnSuccess, fnError, str){
+      exec(fnSuccess, fnError, "Btpnetonline", "print", [str]);
+   },
+   printText: function(fnSuccess, fnError, str){
+      exec(fnSuccess, fnError, "Btpnetonline", "printText", [str]);
+   },
+    printImage: function(fnSuccess, fnError, str){
+      exec(fnSuccess, fnError, "Btpnetonline", "printImage", [str]);
+    },
+   printPOSCommand: function(fnSuccess, fnError, str){
+      exec(fnSuccess, fnError, "Btpnetonline", "printPOSCommand", [str]);
+   }
 };
+
+module.exports = BTPrinter;
